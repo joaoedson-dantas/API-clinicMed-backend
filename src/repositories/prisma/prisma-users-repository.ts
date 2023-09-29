cb80e1f59155cebf7670129789cd709230126244
João Edson <joaoedson.dantas@outlook.com>
2023-09-29 19:41:27 -0300

    feat: created the doctor's repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string): Promise<{
+    id: string
+    name: string
+    login: string
+    password_hash: string
+    created_at: Date
+  } | null> {
+    throw new Error('Method not implemented.')
+  }
+
   async findByLogin(login: string) {
     const user = await prisma.user.findUnique({
       where: {
