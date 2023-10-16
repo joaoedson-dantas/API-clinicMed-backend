699dce20db7989dc8d58ae4bcde88b514e928a05
João Edson <joaoedson.dantas@outlook.com>
2023-10-16 18:06:17 -0300

    feat: Environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,32 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Plese provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
-  setup: async () => {
-    console.log('setup: teste começou')
+  async setup() {
+    // gerando um banco de dados único para cada suite de testes
+    const schema = randomUUID()
+
+    console.log(generateDatabaseURL(schema))
 
     return {
-      teardown: async () => {
+      async teardown() {
         console.log('teardown: teste finalizado')
       },
     }
   },
   transformMode: 'web', // Defina 'transformMode' como 'web' ou 'ssr'
 }
-// async setup() -> qual código eu quero executar antes dos testes, vai executar antes de cada arquivo de teste
