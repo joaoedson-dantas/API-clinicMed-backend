2ce76a9f746f2e6f2229af312bc9097d5554d1c9
João Edson <joaoedson.dantas@outlook.com>
2023-10-17 22:47:47 -0300

    feat: creating controllers and routes for doctors use case

--- src/http/controllers/register-controller.e2e.spec.ts
+++ src/http/controllers/register-controller.e2e.spec.ts
@@ -1,8 +1,14 @@
-/* import { describe, it, beforeAll, afterAll, expect } from 'vitest' */
 import request from 'supertest'
 import { app } from '@/app'
 
 describe('Register (e2e) ', () => {
+  beforeAll(async () => {
+    await app.ready() // garantir que o app esteja pronto
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
   it('should be able to register', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'Jhon Doe',
