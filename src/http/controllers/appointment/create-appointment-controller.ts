8accc566e8212e6c27cb632348ce7f13ab6ec3dd
João Edson <joaoedson.dantas@outlook.com>
2023-10-17 23:07:12 -0300

    feat: added medical appointment creation controller

---
+++ src/http/controllers/appointment/create-appointment-controller.ts
@@ -0,0 +1,35 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { Specialty } from '@prisma/client'
+import { makeAppointmentMedUseCase } from '@/use-cases/factories/make-appointment-med'
+
+export async function createAppointment(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const createAppointmentBodySchema = z.object({
+    patientCPF: z.string().min(11).max(11),
+    doctorId: z.string().optional(),
+    start_time: z.date(),
+    specialty: z.nativeEnum(Specialty),
+  })
+  const { patientCPF, doctorId, start_time, specialty } =
+    createAppointmentBodySchema.parse(request.body)
+
+  try {
+    const appointmentMedUseCase = makeAppointmentMedUseCase()
+    const { query } = await appointmentMedUseCase.execute({
+      patientCPF,
+      doctorId,
+      start_time,
+      specialty,
+    })
+    return reply.status(201).send({
+      query,
+    })
+  } catch (err) {
+    if (err instanceof Error) {
+      return reply.status(409).send({ message: err.message })
+    }
+  }
+}
