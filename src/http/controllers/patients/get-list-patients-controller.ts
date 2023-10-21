d65021c42da41cea4b3739ff002bc85eb37d8878
João Edson <joaoedson.dantas@outlook.com>
2023-10-21 20:06:21 -0300

    feat: added to controller for fetching a list of patients

---
+++ src/http/controllers/patients/get-list-patients-controller.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetListPatientsUseCase } from '@/use-cases/factories/make-get-list-patients'
+
+export async function getListPatients(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const getListPatietsQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+  console.log(getListPatietsQuerySchema.parse(request.query))
+
+  const { page } = getListPatietsQuerySchema.parse(request.query)
+
+  const getListDoctorsUseCase = makeGetListPatientsUseCase()
+  const { patientsOrder } = await getListDoctorsUseCase.execute({
+    page,
+  })
+
+  return reply.status(200).send({
+    patientsOrder,
+  })
+}
